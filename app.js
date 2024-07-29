6715c2d207700fce869713924e753552be63e76f
studhab <studhabapp@gmail.com>
2024-07-29 11:23:57 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// string 
+var username = 'john doe';
+
+// username = 'jane doe'
+
+//Integer
+let age = 32
+
+//Boolean
+let registered = true
+
+//Float
+const balance = 12.34
+
+//Null
+let phone = null
+
+phone = '000111222333'
+
+//Object
+const student = {
+    name: 'john doe',
+    age: 32,
+    registered: false,
+    balance: 12.34,
+    phone: '000111222'
+}
+
+//Array
+const fruits = ['mango', 'pineapple', 'orange']
+
+//pea
+
+//how to remove and insert values into an array
+
+fruits.push('pea')
+
+fruits.pop()
+
+fruits.unshift('cucumber')
+
+fruits.shift()
+
+
+//array of objects 
+const students = [
+    {
+        name: 'john doe',
+        department: 'political science',
+        age: 32
+    },
+    {
+        name: 'mark doe',
+        department: 'information technology'
+    },
+    {
+        name: 'jane corwel',
+        department: 'agriculture'
+    }
+]
+
+//class
+class Cars {
+    constructor(name, model) {
+        this.name = name
+        this.model = model
+    }
+}
+
+car1 = new Cars('Volvo', 2011)
+
+car2 = new Cars('Honda', 2014, )
+
+console.log()
+
